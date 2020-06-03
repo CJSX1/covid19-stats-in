25575c597207d6e5609f5536d4c470162adbe40e
Jaq <suraj@docintosh.com>
2020-06-03 10:32:11 +0530

    minor changes

--- src/App.js
+++ src/App.js
@@ -1,43 +1,35 @@
-import React, { Component } from 'react'
-import { BrowserRouter} from 'react-router-dom';
+import React, { Component } from "react";
+import { BrowserRouter } from "react-router-dom";
 
-import { Navbar, Cards, Charts, StateStats, Infographics } from './components'
-import { fetchData } from './api';
+import { Navbar, Cards, Charts, StateStats, Infographics } from "./components";
+import { fetchData } from "./api";
 
 class App extends Component {
-
   state = {
-    data:{},
-  }
+    data: {},
+  };
 
-  async componentDidMount(){
+  async componentDidMount() {
     const totalData = await fetchData();
     this.setState({ data: totalData });
   }
-  
+
   render() {
-    
     const { data } = this.state;
-
-
 
     return (
       <BrowserRouter>
         <div className="App">
           <Navbar />
 
-          <Cards data={ data } />
-          <Charts data={ data } />
+          <Cards data={data} />
+          <Charts data={data} />
           <StateStats />
           <Infographics />
-
         </div>
       </BrowserRouter>
-    )
+    );
   }
 }
 
-export default App
-
-// https://covid-19india-api.herokuapp.com/v2.0/country_data
-// https://covid-19india-api.herokuapp.com/v2.0/state_data
+export default App;
